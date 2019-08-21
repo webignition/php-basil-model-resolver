ef87ab9933b9d76fe0cdc1408c063148c87437e4
Jon Cram <webignition@gmail.com>
2019-08-21 11:29:04 +0100

    Factor out common unknown element exception logic (#26)

--- src/UnknownPageElementException.php
+++ src/UnknownPageElementException.php
@@ -3,32 +3,20 @@
 namespace webignition\BasilModelResolver;
 
 use webignition\BasilContextAwareException\ContextAwareExceptionInterface;
-use webignition\BasilContextAwareException\ContextAwareExceptionTrait;
-use webignition\BasilContextAwareException\ExceptionContext\ExceptionContext;
 
-class UnknownPageElementException extends \Exception implements ContextAwareExceptionInterface
+class UnknownPageElementException extends UnknownElementException implements ContextAwareExceptionInterface
 {
-    use ContextAwareExceptionTrait;
-
     private $importName;
-    private $elementName;
 
     public function __construct(string $importName, string $elementName)
     {
-        parent::__construct('Unknown page element "' . $elementName . '" in page "' . $importName . '"');
+        parent::__construct($elementName, 'Unknown page element "' . $elementName . '" in page "' . $importName . '"');
 
         $this->importName = $importName;
-        $this->elementName = $elementName;
-        $this->exceptionContext = new ExceptionContext();
     }
 
     public function getImportName(): string
     {
         return $this->importName;
-    }
-
-    public function getElementName(): string
-    {
-        return $this->elementName;
     }
 }
