f3e8f87abebc4509a2f7d977faaa667b4d358ac5
Jon Cram <webignition@gmail.com>
2019-09-12 13:38:01 +0100

    Update to models 0.74 (#32)

--- src/AssertionResolver.php
+++ src/AssertionResolver.php
@@ -3,7 +3,12 @@
 namespace webignition\BasilModelResolver;
 
 use webignition\BasilModel\Assertion\AssertionInterface;
+use webignition\BasilModel\Assertion\ValueComparisonAssertionInterface;
+use webignition\BasilModel\Exception\InvalidAssertionExaminedValueException;
+use webignition\BasilModel\Exception\InvalidAssertionExpectedValueException;
 use webignition\BasilModel\Identifier\IdentifierCollectionInterface;
+use webignition\BasilModel\Value\AssertionExaminedValue;
+use webignition\BasilModel\Value\AssertionExpectedValue;
 use webignition\BasilModelProvider\Exception\UnknownPageException;
 use webignition\BasilModelProvider\Page\PageProviderInterface;
 
@@ -30,6 +35,8 @@ class AssertionResolver
      *
      * @return AssertionInterface
      *
+     * @throws InvalidAssertionExaminedValueException
+     * @throws InvalidAssertionExpectedValueException
      * @throws UnknownElementException
      * @throws UnknownPageElementException
      * @throws UnknownPageException
@@ -39,18 +46,16 @@ class AssertionResolver
         PageProviderInterface $pageProvider,
         IdentifierCollectionInterface $identifierCollection
     ): AssertionInterface {
-        $examinedValue = $assertion->getExaminedValue();
-        if (null !== $examinedValue) {
-            $resolvedValue = $this->valueResolver->resolve($examinedValue, $pageProvider, $identifierCollection);
+        $examinedValue = $assertion->getExaminedValue()->getExaminedValue();
 
-            $assertion = $assertion->withExaminedValue($resolvedValue);
-        }
+        $resolvedValue = $this->valueResolver->resolve($examinedValue, $pageProvider, $identifierCollection);
+        $assertion = $assertion->withExaminedValue(new AssertionExaminedValue($resolvedValue));
 
-        $expectedValue = $assertion->getExpectedValue();
-        if (null !== $expectedValue) {
+        if ($assertion instanceof ValueComparisonAssertionInterface) {
+            $expectedValue = $assertion->getExpectedValue()->getExpectedValue();
             $resolvedValue = $this->valueResolver->resolve($expectedValue, $pageProvider, $identifierCollection);
 
-            $assertion = $assertion->withExpectedValue($resolvedValue);
+            $assertion = $assertion->withExpectedValue(new AssertionExpectedValue($resolvedValue));
         }
 
         return $assertion;
