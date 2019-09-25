50dc6fe6056cc0a007fa8f2cdceaa5d2219dc1e3
Jon Cram <jon@webignition.net>
2019-09-25 15:45:51 +0100

    Update TestResolver

--- src/TestResolver.php
+++ src/TestResolver.php
@@ -3,6 +3,8 @@
 namespace webignition\BasilModelResolver;
 
 use webignition\BasilContextAwareException\ExceptionContext\ExceptionContextInterface;
+use webignition\BasilModel\Exception\InvalidAssertionExaminedValueException;
+use webignition\BasilModel\Exception\InvalidAssertionExpectedValueException;
 use webignition\BasilModel\Identifier\IdentifierCollection;
 use webignition\BasilModel\Test\Test;
 use webignition\BasilModel\Test\TestInterface;
@@ -47,6 +49,8 @@ class TestResolver
      * @return TestInterface
      *
      * @throws CircularStepImportException
+     * @throws InvalidAssertionExaminedValueException
+     * @throws InvalidAssertionExpectedValueException
      * @throws UnknownDataProviderException
      * @throws UnknownElementException
      * @throws UnknownPageElementException
